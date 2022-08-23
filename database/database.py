0d18349bb48c7a244034c38add0d7f4f2d015f7c
theyanaga <felipe@yanaga.com.br>
2022-08-23 16:58:09 -0400

    Hi Claire v2

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://localhost:5432/fast"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
